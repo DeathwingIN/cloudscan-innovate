4fdea5db23c185118d5f2e5b6329c064ebfb4989
Imesh Nirmal <imeshnirmal1u@gmail.com>
2025-02-08 08:33:03 +0530

    Fix scroll issue

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,6 +12,7 @@ const Hero = () => {
             backgroundImage:
                 "url('https://plus.unsplash.com/premium_photo-1663040309722-6c9dbe9a1f0b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
           }}
+
       >
         {/* Overlay */}
         <div className="absolute inset-0 bg-black/50"></div>
