e88bd78accf42fa1229334e71ec635e3aaf6b71e
Imesh Nirmal <imeshnirmal1u@gmail.com>
2025-08-04 12:26:01 +0530

    refactor: Update Hero component layout and styling for improved responsiveness and visual consistency # Done

--- src/components/Hero5.tsx
+++ src/components/Hero5.tsx
@@ -8,18 +8,16 @@ const Hero = () => {
   return (
     <div className="bg-white w-full min-h-screen">
       {/* Desktop Layout */}
-      <div className="hidden lg:flex lg:flex-row lg:justify-center lg:w-full">
-        <div className="bg-white overflow-hidden w-[1440px] h-[827px]">
-          <div className="relative w-[1466px] h-[827px] left-[95px]">
+      <div className="hidden lg:flex lg:flex-row lg:justify-center lg:w-full lg:h-screen">
+        <div className="bg-white overflow-hidden w-[1440px] h-full flex items-center justify-center">
+          <div className="relative w-[1466px] h-[700px] left-[95px]">
             <img
-              className="absolute w-[700px] h-[700px] top-0 left-[600px] object-cover"
+              className="absolute w-[600px] h-[600px] top-14 left-[700px] object-cover"
               alt="Element"
               src={x0001}
             />
 
-            <div className="absolute w-[1466px] h-[117px] top-[669px] left-0 bg-[#ffde59] rounded-[30px]" />
-
-            <p className="absolute w-[630px] top-[218px] left-8 [font-family:'Inter-Regular',Helvetica] font-normal text-black text-5xl tracking-[0] leading-[normal]">
+            <p className="absolute w-[630px] top-[150px] left-8 [font-family:'Inter-Regular',Helvetica] font-normal text-black text-5xl tracking-[0] leading-[normal]">
               Boost Efficiency with
               <br />
               Smart Tech &amp;
@@ -27,29 +25,31 @@ const Hero = () => {
               Custom Solutions
             </p>
 
-            <div className="absolute w-[663px] h-[55px] top-[430px] left-8 bg-[#00abff] rounded-[10px]" />
+            <div className="absolute w-[663px] h-[55px] top-[340px] left-8 bg-[#00abff] rounded-[10px]" />
 
-            <p className="absolute top-[442px] left-[84px] [font-family:'Inter-Regular',Helvetica] font-normal text-white text-2xl tracking-[0] leading-[normal]">
+            <p className="absolute top-[352px] left-[84px] [font-family:'Inter-Regular',Helvetica] font-normal text-white text-2xl tracking-[0] leading-[normal]">
               Smarter Scanning. Faster Operations. Zero Errors
             </p>
 
-            <img
-              className="absolute w-[277px] h-[152px] top-[652px] left-[23px] object-cover"
-              alt="Element"
-              src={x11}
-            />
-
-            <img
-              className="absolute w-[197px] h-[113px] top-[674px] left-[588px] object-cover"
-              alt="Element"
-              src={x31}
-            />
-
-            <img
-              className="absolute w-[129px] h-[124px] top-[669px] left-[341px] object-cover"
-              alt="Element"
-              src={x21}
-            />
+            <div className="absolute w-[1466px] h-[100px] top-[550px] left-0 bg-[#ffde59] rounded-[30px] flex items-center justify-start gap-8 px-12">
+              <img
+                className="w-[200px] h-[80px] object-cover"
+                alt="Element"
+                src={x11}
+              />
+              
+              <img
+                className="w-[120px] h-[80px] object-cover"
+                alt="Element"
+                src={x21}
+              />
+              
+              <img
+                className="w-[180px] h-[80px] object-cover"
+                alt="Element"
+                src={x31}
+              />
+            </div>
           </div>
         </div>
       </div>
@@ -81,7 +81,7 @@ const Hero = () => {
 
         {/* Main Hero Image Section - Right Corner */}
         <div className="flex-1 flex items-end justify-end">
-          <div className="relative w-96 sm:w-56 md:w-64">
+          <div className="relative w-96 sm:w-[750px] md:w-64">
             <img
               className="w-full h-auto object-cover"
               alt="Element"
